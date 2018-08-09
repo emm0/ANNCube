d4f92135b2e6057ec653d53e24b5977be1f4aa2e
em_m0 <em_m0@LAPTOP-FLP3HJN3>
2018-08-09 21:43:41 +0200

    First approximation of the AnnCube interface

--- anncube/src/main/java/org/cynemi/anncube/interfaces/AnnCube.java
+++ anncube/src/main/java/org/cynemi/anncube/interfaces/AnnCube.java
@@ -9,6 +9,39 @@ package org.cynemi.anncube.interfaces;
  */
 public interface AnnCube {
 	
+	/**
+	 * every cycle is a tick, in a tick we process everything
+	 */
+	void tick();
 	
+	/**
+	 * Stimulate the neuron(x,y,z) with stimulus
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 * @param z z coordinate
+	 * @param stimulus value of the stimulus
+	 */
+	void stimulate(int x, int y, int z, int stimulus);
+	
+	/**
+	 * Retrieve the stimulus in neuron(x,y,z)
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return
+	 */
+	int getOutputStimulus(int x, int y, int z);
+	
+	/**
+	 * Gives to the system pleasureAmount of pleasure
+	 * @param pleasureAmount
+	 */
+	void givesPleasure(int pleasureAmount);
+	
+	/**
+	 * Gives to the system painAmount of pain
+	 * @param painAmount
+	 */
+	void givesPain(int painAmount);
 
 }
