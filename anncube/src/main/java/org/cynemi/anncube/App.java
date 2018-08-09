1687d8a8fae64dd1de280d1bbb3dd0b81ea15a72
em_m0 <em_m0@LAPTOP-FLP3HJN3>
2018-08-09 21:14:05 +0200

    First committtttt !!!!! :^)

---
+++ anncube/src/main/java/org/cynemi/anncube/App.java
@@ -0,0 +1,13 @@
+package org.cynemi.anncube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Neural World!" );
+    }
+}
---
+++ anncube/src/main/java/org/cynemi/anncube/interfaces/AnnCube.java
@@ -0,0 +1,14 @@
+package org.cynemi.anncube.interfaces;
+
+/**
+ * An AnnCube must implement the behavior of a cube of Artificial Neural Network
+ * 
+ * 3D shape, pleasure and pain, growing connections
+ * @author em_m0
+ *
+ */
+public interface AnnCube {
+	
+	
+
+}
